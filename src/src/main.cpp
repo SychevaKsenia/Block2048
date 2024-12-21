a6158d96270040ec51df8f4f8d2a807f82b6497c
Sycheva Ksenia <Ksenia-2006@yandex.ru>
2024-12-22 02:12:24 +0700

    Add block to gameBoard

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,22 +1,40 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
+#include "../headers/block.hpp"
+#include <iostream>
+using namespace sf;
+
 int main()
 {
-    sf::RenderWindow window({1920u, 1080u}, "CMake SFML Project");
+    sf::RenderWindow window({600u, 700u}, "Block2028");
     window.setFramerateLimit(144);
+    sf::Texture block_texture;
+    block_texture.loadFromFile("source/cat1.jpg");
+
+    Block *block = new Block(block_texture, sf::Vector2f(250, 100), 2);
+    // Block *block1 = new Block(block_texture, sf::Vector2f(250, 400), 2);
 
     while (window.isOpen())
     {
-        for (auto event = sf::Event(); window.pollEvent(event);)
+        float time = 300.f;
+        sf::Event event;
+        while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
             {
                 window.close();
             }
         }
+        Color color(255, 165, 0);
+        block->Update(time);
+        // std::cout << time << std::endl;
 
-        window.clear();
+        window.clear(sf::Color::White);
+
+        window.draw(block->getSprite());
+        // window.draw(block1->getSprite());
+
         window.display();
     }
 }
