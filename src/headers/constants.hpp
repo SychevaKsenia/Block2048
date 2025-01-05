320e9258a5f40a24e9c6850c52032216689f1e7d
Sycheva Ksenia <Ksenia-2006@yandex.ru>
2025-01-06 01:32:08 +0700

    Create file with constants

---
+++ src/headers/constants.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+constexpr unsigned int WINDOW_HEIGHT = 700;
+constexpr unsigned int WINDOW_WEIGHT = 600;
+
+constexpr int SIZE_BOARD_WEIGHT = 5;
+constexpr int SIZE_BOARD_HEIGHT = 6;
+
+constexpr int START_X = 250;
+constexpr int START_Y = 100;
+
+constexpr float PLAYER_SPEED = 0.12;
+constexpr int SIZE_BLOCK = 100;
+
+constexpr int START_VALUE_BLOCK = 2;
